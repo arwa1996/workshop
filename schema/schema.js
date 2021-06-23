e0dad08b8ffee763efc869e951c4f808f8a71721
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2021-06-23 14:18:48 +0200

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,27 @@
+const import { compose, graphql,GraphQLString ,GraphQLInt} from 'react-apollo'
+const graphql = require('graphql');
+const  GraphQlObjectType = graphql;
+
+const UserType = new GraphQlObjectType ({
+    name: 'User',
+    fields:{
+        id:{type: GraphQLString},
+        firstName:{type: GraphQLString},
+        age:{type: GraphQLInt}
+    }
+})
+
+// root 
+
+const RootQuery = new GraphQlObjectType ({
+    name: 'RootQueryType',
+    fields:{
+        user:{type: UserType,
+        args: {id:{type: GraphQLString}},
+        resolved:(parentValue,args) {
+return _.find(users,{id:args.id})
+    }
+    },
+       
+    }
+})
